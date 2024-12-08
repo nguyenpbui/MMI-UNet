b04ea3bd1de72fa64ca73f82577dbb92f64ab947
nguyenpbui <34409468+nguyenpbui@users.noreply.github.com>
2024-12-08 12:59:51 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,79 @@
+# LanGuideMedSeg-MICCAI2023
+Pytorch code of MICCAI 2023 Paper-**Visual-Textual Matching Attention for Lesion Segmentation in Chest Images**
+(Early Acceptance-Top 11%)
+
+MICCAI 2023 Conference paper: [https://link.springer.com/chapter/10.1007/978-3-031-72114-4_67)
+
+
+## Framework
+
+![Framework](./figs/mmi-unet.png)
+
+## Requirements
+1. Environment  
+The main mandatory dependency versions are as follows:  
+    ```
+    python=3.10.15  
+    torch=2.5.0+cu12.1  
+    torchvision=0.13.1  
+    pytorch_lightning=1.9.0  
+    torchmetrics=0.10.3  
+    transformers=4.24.0  
+    monai=1.0.1  
+    pandas  
+    einops  
+    ```
+
+2. (Option) Download the pretrained model of CXR-BERT and ConvNeXt
+   
+   CXR-BERT-specialized see: https://huggingface.co/microsoft/BiomedVLP-CXR-BERT-specialized/tree/main  
+   ConvNeXt-tiny see: https://huggingface.co/facebook/convnext-tiny-224/tree/main
+
+   Download the file 'pytorch_model.bin' to './lib/BiomedVLP-CXR-BERT-specialized/' and './lib/convnext-tiny-224'
+
+   Or just use these models online:
+   ```
+   url = "microsoft/BiomedVLP-CXR-BERT-specialized"
+   tokenizer = AutoTokenizer.from_pretrained(url,trust_remote_code=True)
+   model = AutoModel.from_pretrained(url, trust_remote_code=True)
+   ```
+   
+
+## Dataset
+1. QaTa-COV19 Dataset(images & segmentation mask)  
+    QaTa-COV19 Dataset See Kaggle: [https://www.kaggle.com/datasets/aysendegerli/qatacov19-dataset](https://www.kaggle.com/datasets/aysendegerli/qatacov19-dataset)
+
+    **We use QaTa-COV19-v2 in our experiments.**
+
+2. QaTa-COV19 Text Annotations(from thrid party)  
+    Check out the related content in LViT: [https://github.com/HUANGLIZI/LViT](https://github.com/HUANGLIZI/LViT)
+
+    **Thanks to Li et al. for their contributions. If you use this dataset, please cite their work.**
+
+## QuickStart
+Our training is implemented based on PyTorch Lightning. 
+Please check the relevant training settings in train.py and config.  
+
+For example:
+```train_csv_path:./data/QaTa-COV19-v2/prompt/train.csv```
+
+To train a model, please execute:  
+```python train.py```  
+To evaluate a model, please excute:  
+```python evaluate.py```
+
+## Citation
+
+If you find our work useful in your research, please consider citing:
+```
+@inproceedings{zhong2023ariadne,
+  title={Ariadne’s Thread: Using Text Prompts to Improve Segmentation of Infected Areas from Chest X-ray Images},
+  author={Zhong, Yi and Xu, Mengqiu and Liang, Kongming and Chen, Kaixin and Wu, Ming},
+  booktitle={International Conference on Medical Image Computing and Computer-Assisted Intervention},
+  pages={724--733},
+  year={2023},
+  organization={Springer}
+}
+```
+## Acknowledgements
+MMI-UNet is built based on GuideDecoder. We thank thir authors for making the source code publicly available.
---
+++ train.py
@@ -0,0 +1,95 @@
+import torch
+from torch.utils.data import DataLoader
+from utils.dataset import QaTa
+import utils.config as config
+from torch.optim import lr_scheduler
+from engine.wrapper import *
+
+import pytorch_lightning as pl    
+from torchmetrics import Accuracy,Dice
+from torchmetrics.classification import BinaryJaccardIndex
+from pytorch_lightning.callbacks import ModelCheckpoint,EarlyStopping
+
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Language-guide Medical Image Segmentation')
+    parser.add_argument('--config',
+                        default='./config/training.yaml',
+                        type=str,
+                        help='config file')
+
+    args = parser.parse_args()
+    assert args.config is not None
+    cfg = config.load_cfg_from_cfg_file(args.config)
+
+    return cfg
+
+
+if __name__ == '__main__':
+
+    args = get_parser()
+    print("cuda:",torch.cuda.is_available())
+
+    ds_train = QaTa(csv_path=args.train_csv_path,
+                    root_path=args.train_root_path,
+                    tokenizer=args.bert_type,
+                    image_size=args.image_size,
+                    mode='train')
+
+    ds_valid = QaTa(csv_path=args.valid_csv_path,
+                    root_path=args.valid_root_path,
+                    tokenizer=args.bert_type,
+                    image_size=args.image_size,
+                    mode='valid')
+
+
+    dl_train = DataLoader(ds_train, batch_size=args.train_batch_size, shuffle=True, num_workers=args.train_batch_size)
+    dl_valid = DataLoader(ds_valid, batch_size=args.valid_batch_size, shuffle=False, num_workers=args.valid_batch_size)
+    
+    if args.model_save_filename == "medseg":
+        model = LanGuideMedSegWrapper(args)
+
+    elif args.model_save_filename == "mmiunet":
+        model = MMIUNet_Wrapper(args)
+
+    elif args.model_save_filename == "mmiunext_s":
+        model = MMIUNeXt_S_Wrapper(args)
+        
+    else:
+        model = MMIUNet_GS_Wrapper(args)
+
+    ## 1. setting recall function
+    model_ckpt = ModelCheckpoint(
+        dirpath=args.model_save_path,
+        filename=args.model_save_filename,
+        monitor='val_loss',
+        save_top_k=1,
+        mode='min',
+        verbose=True,
+    )
+
+    early_stopping = EarlyStopping(monitor = 'val_loss',
+                            patience=args.patience,
+                            mode = 'min'
+    )
+
+    ## 2. setting trainer
+
+    trainer = pl.Trainer(logger=True,
+                        min_epochs=args.min_epochs,max_epochs=args.max_epochs,
+                        accelerator='gpu', 
+                        devices=args.device,
+                        callbacks=[model_ckpt,early_stopping],
+                        enable_progress_bar=False,
+                        ) 
+
+    ## 3. start training
+    print('start training')
+    trainer.fit(model,dl_train,dl_valid)
+    print('done training')
+
